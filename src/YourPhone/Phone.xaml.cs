c9137df4534fa813e5f17ba7e81f3e9154e17caa
Bruno Zell <bruno.zzell@gmail.com>
2019-11-14 05:06:28 +0100

    add basic phone WPF

---
+++ src/YourPhone/Phone.xaml.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YourPhone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PinTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
